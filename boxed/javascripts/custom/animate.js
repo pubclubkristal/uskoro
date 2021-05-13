efeb5c6faa6b1f47c833bf422e0c10c4553d34f2
pubclubkristal <todorovicd939@gmail.com>
2021-05-13 14:24:19 +0200

    Update

--- boxed/javascripts/custom/animate.js
+++
@@ -1,7 +0,0 @@
-
-jQuery(document).ready(function() {
-	jQuery('.animated').addClass("hidden").viewportChecker({
-	    classToAdd: 'visible animated bounceInLeft', // Class to add to the elements when they are visible
-	    offset: 100    
-	   });   
-});    
