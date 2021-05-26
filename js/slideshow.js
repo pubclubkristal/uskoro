2afbfa573906b24d530e6b1a263ea1684d151de4
pubclubkristal <todorovicd939@gmail.com>
2021-05-26 21:10:26 +0200

    publi

--- js/slideshow.js
+++
@@ -1,9 +0,0 @@
-$(".slideshow-bg main.background").vegas({
-	delay: 3000,
-    slides: [
-        { src: "images/bg-1.jpg" },
-        { src: "images/bg-2.jpg" },
-        { src: "images/bg-3.jpg" },
-        { src: "images/bg-4.jpg" }
-    ]
-});
